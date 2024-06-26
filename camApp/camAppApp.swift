5de4ea6507c8391af185d68239aecda567471674
anna-khu <annakhulup@gmail.com>
2024-06-26 13:54:07 -0700

    Initial Commit

---
+++ camApp/camAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  camAppApp.swift
+//  camApp
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct camAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
